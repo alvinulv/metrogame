37b122861eb275f3181178266ff7568a882f8a2d
Gustav Wind Just <117112178+MimiqTheKitten@users.noreply.github.com>
2025-12-15 11:21:28 +0100

    Changed route logic

--- Assets/Scripts/Routes/Route logic.cs
+++ Assets/Scripts/Routes/Route logic.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -6,7 +7,7 @@ using UnityEngine;
 public class Routelogic : MonoBehaviour
 {
     LineRenderer lR;
-    public List<GameObject> rWp = new List<GameObject>();
+    public List<Vector3> rWp = new List<Vector3>();
     [SerializeField] GameObject baseWaypoint;
     [SerializeField] bool loop;
     [Header("Debug")]
@@ -21,36 +22,29 @@ public class Routelogic : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (updateRoute)
-        {
-            updateRoute = false;
-            UpdateRoute();
-        }
         if (Input.GetMouseButtonUp(0))
         {
             Vector3 p = Input.mousePosition;
             Vector3 pos = Camera.main.ScreenToWorldPoint(p);
-            AddRouteWaypoint(new Vector3(pos.x,pos.y,0));
-            UpdateRoute();
+            AddRouteWaypoint(RoundedVector(pos));
         }
-        if (Input.GetMouseButton(1))
+        if (Input.GetMouseButton(0))
         {
             Vector3 p = Input.mousePosition;
             Vector3 pos = Camera.main.ScreenToWorldPoint(p);
-            if (rWp[0] != null)
-            {
-                rWp.Insert(0, null);
-                UpdateRoute();
-            }
-            lR.SetPosition(0, pos);
-            UpdateRoute();
+            ChangePos(rWp.Count - 1, RoundedVector(pos));
         }
         if (Input.GetMouseButtonUp(1))
         {
             Vector3 p = Input.mousePosition;
             Vector3 pos = Camera.main.ScreenToWorldPoint(p);
-            AddRouteWaypoint(new Vector3(pos.x, pos.y, 0), 0);
-            UpdateRoute();
+            AddRouteWaypoint(RoundedVector(pos), 0);
+        }
+        if (Input.GetMouseButton(1))
+        {
+            Vector3 p = Input.mousePosition;
+            Vector3 pos = Camera.main.ScreenToWorldPoint(p);
+            ChangePos(0, RoundedVector(pos));
         }
         if (removeFirst)
         {
@@ -65,47 +59,28 @@ public class Routelogic : MonoBehaviour
     }
     void AddRouteWaypoint(Vector3 pos)
     {
-        if (rWp[rWp.Count - 1] == null)
-        {
-            rWp[rWp.Count - 1] = Instantiate(baseWaypoint, pos, transform.rotation, transform);
-        }
-        else
-        {
-            rWp.Add(Instantiate(baseWaypoint, pos, transform.rotation, transform));
-        }
-    }
-    void AddRouteWaypoint(Vector3 pos, int index){
-        if (rWp[index] == null)
-        {
-            rWp[index] = Instantiate(baseWaypoint, pos, transform.rotation, transform);
-        }
-        else
-        {
-            rWp.Insert(index, Instantiate(baseWaypoint, pos, transform.rotation, transform));
-        }
-    }
-    void DestroyWaypoint()
-    {
-
-    }
-    void UpdateRoute(){
+        rWp.Add(pos);
         lR.positionCount = rWp.Count;
-        if (rWp.Count < 2)
-            return;
-        for(int i = 0; i < lR.positionCount; i++)
-        {
-            lR.SetPosition(i, rWp[i].transform.position);
-        }
-        if (loop)
-        {
-            lR.positionCount++;
-            lR.SetPosition(lR.positionCount - 1, lR.GetPosition(0));
-        }
+        lR.SetPositions(rWp.ToArray());
+    }
+    void AddRouteWaypoint(Vector3 pos, int index)
+    {
+        rWp.Insert(index, pos);
+        lR.positionCount = rWp.Count;
+        lR.SetPositions(rWp.ToArray());
     }
     void RemovePoint(int index)
     {
-
         rWp.RemoveAt(index);
-        UpdateRoute();
+        lR.SetPositions(rWp.ToArray());
+    }
+    Vector3 RoundedVector(Vector3 pos)
+    {
+        return new Vector3(Mathf.RoundToInt(pos.x), Mathf.RoundToInt(pos.y), 0);
+    }
+    void ChangePos(int index, Vector3 pos)
+    {
+        rWp[index] = pos;
+        lR.SetPositions(rWp.ToArray());
     }
 }
