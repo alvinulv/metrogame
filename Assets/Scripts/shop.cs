19e6eae18e368a77d3732b2ddd87c094cb9a6f67
thom989t <149476657+thom989t@users.noreply.github.com>
2026-01-19 14:31:52 +0100

    displaying the price isnt working so i commented it out

--- Assets/Scripts/shop.cs
+++ Assets/Scripts/shop.cs
@@ -6,6 +6,11 @@ using UnityEngine.UI;
 
 public class shop : MonoBehaviour
 {
+    public GameObject priceDisplay1;
+    public GameObject priceDisplay2;
+    public GameObject priceDisplay3;
+    public GameObject priceDisplay4;
+    public GameObject priceDisplay5;
     public static int money;
     EventSystem eventSystem;
     int upgradePrice1 = 50;
@@ -17,6 +22,7 @@ public class shop : MonoBehaviour
     void Start()
     {
         eventSystem = FindFirstObjectByType<EventSystem>();
+        //priceDisplay1.GetComponent<Text>().text = upgradePrice1.ToString();
     }
 
     // Update is called once per frame
@@ -24,49 +30,44 @@ public class shop : MonoBehaviour
     {
         
     }
-    public void upgrade1()
-    {
-        upgradePrice1++;
-        float tempUpgradePrice1 = upgradePrice1;
-        upgradePrice1 = (int)(upgradePrice1 * 1.2f);
-        Debug.Log(upgradePrice1);
-
-
-    }
     public void MoreRoutes()
     {
-        if (money > upgradePrice1)
+        if (money >= upgradePrice1)
         {
             money =- upgradePrice1;
             Routelogic.AddRoute();
             upgradePrice1 = (int)(upgradePrice1 * 1.2f);
+            priceDisplay1.GetComponent<Text>().SetText(upgradePrice1.ToString());
         }
         eventSystem.SetSelectedGameObject(null);
     }
     public void MoreTrains()
     {
-        if (money > upgradePrice2)
+        if (money >= upgradePrice2)
         {
             money =- upgradePrice2;
             upgradePrice2 = (int)(upgradePrice2 * 1.2f);
+           // priceDisplay2.GetComponent<Text>().text = upgradePrice2.ToString();
         }
         eventSystem.SetSelectedGameObject(null);
     }
     public void FasterTrains()
     {
-        if (money > upgradePrice3)
+        if (money >= upgradePrice3)
         {
             money =- upgradePrice3;
             upgradePrice3 = (int)(upgradePrice3 * 1.2f);
+           // priceDisplay3.GetComponent<Text>().text = upgradePrice3.ToString();
         }
         eventSystem.SetSelectedGameObject(null);
     }
     public void BiggerTrains()
     {
-        if (money > upgradePrice4)
+        if (money >= upgradePrice4)
         {
             money =- upgradePrice4;
             upgradePrice4 = (int)(upgradePrice4 * 1.2f);
+            //priceDisplay4.GetComponent<Text>().text = upgradePrice4.ToString();
         }
         eventSystem.SetSelectedGameObject(null);
     }
@@ -85,6 +86,7 @@ public class shop : MonoBehaviour
                 Stations.NextPersonCanSpawnMax--;
             }
             upgradePrice5 = (int)(upgradePrice5 * 1.2f);
+            //priceDisplay5.GetComponent<Text>().text = upgradePrice5.ToString();
         }
         eventSystem.SetSelectedGameObject(null);
     }
